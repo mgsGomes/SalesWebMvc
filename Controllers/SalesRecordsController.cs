62470215e9cbe55368d9fe3c44352f7cd2e66a13
Micael de Souza <micaelmgs@gmail.com>
2024-10-15 11:21:29 -0300

    - Adicionado o filtro de departamento da consulta agrupada.

--- Controllers/SalesRecordsController.cs
+++ Controllers/SalesRecordsController.cs
@@ -1,4 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SalesWebMvc.Data;
+using SalesWebMvc.Models;
+using SalesWebMvc.Models.ViewModels;
 using SalesWebMvc.Services;
 using System;
 using System.Collections.Generic;
@@ -9,16 +13,25 @@ namespace SalesWebMvc.Controllers
 {
     public class SalesRecordsController : Controller
     {
+        private readonly SalesWebMvcContext _context;
         private readonly SalesRecordService _salesRecordService;
 
-        public SalesRecordsController(SalesRecordService salesRecordService)
+        public SalesRecordsController(SalesWebMvcContext context, SalesRecordService salesRecordService)
         {
+            _context = context;
             _salesRecordService = salesRecordService;
         }
 
         public IActionResult Index()
         {
-            return View();
+            var departments = _context.Department.OrderBy(x => x.Name).ToList();
+            var allDepartments = new Department { Id = 0, Name = "(Todos)" };
+
+            departments.Insert(0, allDepartments);
+
+            var viewModel = new SalesFormViewModel { Departments = departments };
+
+            return View(viewModel);
         }
 
         public async Task<IActionResult> SimpleSearch(DateTime? minDate, DateTime? maxDate)
@@ -40,7 +53,7 @@ namespace SalesWebMvc.Controllers
             return View(result);
         }
 
-        public async Task<IActionResult> GroupingSearch(DateTime? minDate, DateTime? maxDate)
+        public async Task<IActionResult> GroupingSearch(DateTime? minDate, DateTime? maxDate, int departmentId)
         {
             if (!minDate.HasValue)
             {
@@ -55,8 +68,15 @@ namespace SalesWebMvc.Controllers
             ViewData["minDate"] = minDate.Value.ToString("yyyy-MM-dd");
             ViewData["maxDate"] = maxDate.Value.ToString("yyyy-MM-dd");
 
-            var result = await _salesRecordService.FindByDateGroupingAsync(minDate, maxDate);
-            return View(result);
+            var departments = await _context.Department.OrderBy(x => x.Name).ToListAsync();
+            var allDepartments = new Department { Id = 0, Name = "(Todos)" };
+            departments.Insert(0, allDepartments);
+
+            var groupedSalesRecords = await _salesRecordService.FindByDateGroupingAsync(minDate, maxDate, departmentId);
+
+            var viewModel = new GroupingSearchViewModel { Departments = departments, GroupedSalesRecords = groupedSalesRecords, SelectedDepartmentId = departmentId };
+
+            return View(viewModel);
         }
     }
 }
---
+++ Models/ViewModels/SalesFormViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMvc.Models.ViewModels
+{
+    public class SalesFormViewModel
+    {
+        public ICollection<Department> Departments { get; set; }
+    }
+}
