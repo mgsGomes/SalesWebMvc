526b1f7ebd62a7c752737486b70093d1ad52b16f
Micael de Souza <micaelmgs@gmail.com>
2024-10-22 23:48:56 -0300

    - Criado o cadastro de Tipos de Impedimentos.

--- Controllers/TiposImpedimentosController.cs
+++ Controllers/TiposImpedimentosController.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SalesWebMvc.Data;
+using SalesWebMvc.Models;
 using SalesWebMvc.Services;
 using System;
 using System.Collections.Generic;
@@ -9,10 +12,12 @@ namespace SalesWebMvc.Controllers
 {
     public class TiposImpedimentosController : Controller
     {
+        private readonly SalesWebMvcContext _context;
         private readonly TipoImpedimentoService _tipoImpedimentoService;
 
-        public TiposImpedimentosController(TipoImpedimentoService tipoImpedimentoService)
+        public TiposImpedimentosController(SalesWebMvcContext context, TipoImpedimentoService tipoImpedimentoService)
         {
+            _context = context;
             _tipoImpedimentoService = tipoImpedimentoService;
         }
 
@@ -20,6 +25,104 @@ namespace SalesWebMvc.Controllers
         {
             var list = await _tipoImpedimentoService.FindAllAsync();
             return View(list);
+        }
+
+        public IActionResult Create()
+        {            
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Impedimento")] TipoImpedimento tipoImpedimento)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(tipoImpedimento);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(tipoImpedimento);
+        }
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var tipoImpedimento = await _context.TipoImpedimento.FindAsync(id);
+            if (tipoImpedimento == null)
+            {
+                return NotFound();
+            }
+            return View(tipoImpedimento);
+        }
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Impedimento")] TipoImpedimento tipoImpedimento)
+        {
+            if (id != tipoImpedimento.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(tipoImpedimento);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TipoImpedimentoExists(tipoImpedimento.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(tipoImpedimento);
+        }
+        
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var tipoImpedimento = await _context.TipoImpedimento
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (tipoImpedimento == null)
+            {
+                return NotFound();
+            }
+
+            return View(tipoImpedimento);
+        }
+
+        
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var tipoImpedimento = await _context.TipoImpedimento.FindAsync(id);
+            _context.TipoImpedimento.Remove(tipoImpedimento);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool TipoImpedimentoExists(int id)
+        {
+            return _context.TipoImpedimento.Any(e => e.Id == id);
         }
     }
 }
